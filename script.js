d17809206fbc226fef95fcb30bf4179ca6024322
lucas souza <lucassouzaluciano12@gmail.com>
2024-01-31 20:40:45 -0300

    ...

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const botaoPlayPause = document.getElementById('play-pause')
+const botaoPlayPause = document.getElementById('play-pause');
+const audioCapitulo = document.getElementById('audio-capitulo');
 
 const numeroCapitulos = 10;
 
+function tocarFaixa() {
+    audioCapitulo.play();
+}
