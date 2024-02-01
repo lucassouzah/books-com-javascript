e409ad307ecededce4e83981fa3661eecb080fae
lucas souza <lucassouzaluciano12@gmail.com>
2024-02-01 18:57:34 -0300

    update 1

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ const botaoPlayPause = document.getElementById('play-pause');
 const audioCapitulo = document.getElementById('audio-capitulo');
 
 const numeroCapitulos = 10;
+let taTocando = 0;
 
 function tocarFaixa() {
     audioCapitulo.play();
 }
+
+function pausarFaixa() {
+    audioCapitulo.pause();
+}
+
+function tocarOuPausar() {
+    if (taTocando === 0) {
+        tocarFaixa();
+    }else{
+        pausarFaixa();
+    }
+
+}
+
+botaoPlayPause.addEventListener('click', tocarFaixa);
+
